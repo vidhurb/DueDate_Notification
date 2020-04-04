6f40683ad41cb2d685b5e1be5b67cbb102e9474a
Vidhur <vidhur@ymail.com>
2020-04-04 12:30:31 +0530

    Added config file for api keys.
    Added cofnig fle, Support for phone numbers

--- notifier.py
+++ notifier.py
@@ -1,40 +1,42 @@
 #! /usr/bin/python3 
 import openpyxl
-from openpyxl import Workbook
+ #Create a file called config.py and add acesss token in thew same dir, it will be ignored by git for security
+import config
 import datetime
+from openpyxl import Workbook
 from datetime import datetime, timedelta, date
 
 wb_fileName = 'Forum_DB.xlsx'
 
 #In the First
 def main():
-	print("Hello World")
 	wb = Workbook()
 	sheet = wb.active
 	wb = openpyxl.load_workbook(wb_fileName)
 	get_date(wb)
+	print('From Config: ', config.access_token)
 
 #Send Notifications
-def send_notification(due_date, noOfDays):
+def send_notification(due_date, noOfDays, phoneNum):
 	if noOfDays >= 0:
-		msg = "Kindly note that your subcription expires on " + str(due_date.date()) +", Kindly renew."
+		msg = "Kindly note that your subcription expires on " + str(due_date.date()) +", Kindly renew." + "\nPhone: " + phoneNum
 	else:
-		msg = "Kindly note that your subcription has already expired on " + str(due_date.date()) +", Kindly renew."
+		msg = "Kindly note that your subcription has already expired on " + str(due_date.date()) +", Kindly renew." + "\nPhone: " + phoneNum
 	print(msg)
 
 def get_date(wb):
-	startRow, endRow, DueDateCol = 2, 8, 3
+	startRow, endRow, DueDateCol, phoneNumCol = 2, 8, 3, 4
 	current = wb.active
 
 	for i in range(startRow, endRow, 1):
 		due_date = datetime.strptime(str(current.cell(row = i, column = DueDateCol).value), '%Y-%m-%d %H:%M:%S')
+		phoneNum = str(current.cell(row = i, column = phoneNumCol).value)
 		today = datetime.today()
 		ndays = (due_date - today).days
-		print('Due Date: ', due_date)
-		print('days: ', ndays)
+		#print('Due Date: ', due_date)
+		#print('days: ', ndays)
 		if ndays <= 3:
-			send_notification(due_date, ndays)
-
+			send_notification(due_date, ndays, phoneNum)
 
 if __name__=="__main__":
 	main()
